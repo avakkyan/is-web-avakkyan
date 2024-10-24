0d45a5b47b187b2b25403bf86ea1dddefe0a7088
avakkyan <avakkyan@gmail.com>
2024-10-24 14:41:23 +0300

    add loadtime

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-(function () {
-    // Event listener for page load
-    window.addEventListener('load', function () {
-        // Calculate and display page load time
-        const loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-        const footer = document.querySelector('footer');
-        if (footer) {
-            const loadInfo = document.createElement('p');
-            loadInfo.textContent = `Page load time: ${loadTime} ms`;
-            footer.appendChild(loadInfo);
-        }
-    });
-})();
+window.addEventListener("load", function () {
+    const loadTime = performance.now();
+    document.getElementById("load-stats").textContent = `Старница загружена за: ${Math.round(loadTime)} мс`;
+});
